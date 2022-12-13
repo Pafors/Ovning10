892c8adc8ca34b8089625429484404d24b3c3344
Patric Fors <patric.fors@outlook.com>
2022-12-13 16:12:55 +0100

    Initial commit of first code

---
+++ 1. Star Wars Biometrics/sw.js
@@ -0,0 +1,100 @@
+// API URI
+const starWarsApiUrl = 'https://www.swapi.tech/api/people/';
+
+// Get reference on where to place the result
+const display = document.querySelector('#displayResult');
+
+// Get reference to the form
+const form = document.querySelector('#inputForm');
+
+// Add event listener for the nodes in the form
+form.addEventListener('submit', (e) => searchForCharacter(e));
+
+
+// Event handler for the input form
+function searchForCharacter(e) {
+    // Stop event bubbling up, mostly for "submit" button
+    e.preventDefault();
+
+    // Get the new item from the form, remove empty spaces
+    const character = form['sw-character-field'].value.trim();
+
+    switch (e.target.id) {
+        // Submit button is pressed
+        case 'inputForm':
+            // Skip empty entries
+            if (character === '') { e.target.reset(); return; }
+
+            // Get the data
+            getApi(`${starWarsApiUrl}?name=${character}`);
+
+            // Clear the input form
+            e.target.reset();
+            break;
+        default:
+            // Inform that something that is unhandled was received
+            console.log(`*** UNHANDLED TARGET: ${e.target}`);
+            break;
+    }
+}
+
+function getApi(fullUri) {
+    displayInfo("(hämtar data)");
+    fetch(fullUri)
+        .then(res => res.json())
+        .then(data => {
+            displayResult(data);
+        })
+        .catch(err => console.log(err))
+}
+
+
+// Simple display of info
+function displayInfo(infoText) {
+    // Clear previous result
+    display.innerHTML = '';
+    // Display info message
+    display.innerHTML = infoText;
+}
+
+// Display the received array in the display area
+function displayResult(items) {
+    console.log(items);
+    console.log(items.result);
+    // Clear previous result
+    display.innerHTML = '';
+
+    // If empty, inform user
+    if (items.result.length === 0) {
+        displayInfo('(inget resultat)');
+        return;
+    }
+
+    // Iterate through all items and add them
+    items.result.forEach((character) => {
+        // Add item card to row
+        display.innerHTML += createCard(character);
+    });
+
+}
+
+// Item bootstrap card factory function
+function createCard(character) {
+    const card = 
+`Description: ${character.description}
+birth_year: ${character.properties.birth_year}
+created: ${character.properties.created}
+edited: ${character.properties.edited}
+eye_color: ${character.properties.eye_color}
+gender: ${character.properties.gender}
+hair_color: ${character.properties.hair_color}
+height: ${character.properties.height}
+homeworld: ${character.properties.homeworld}
+mass: ${character.properties.mass}
+name: ${character.properties.name}
+skin_color: ${character.properties.skin_color}
+url: ${character.properties.url}
+
+`;
+   return card;
+}
